d8494206a743a55ffc75a18441189679591b2b60
Aina Niemetz <aina.niemetz@gmail.com>
2019-10-11 11:44:34 -0700

    Fix test setup for Travis.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,12 +5,17 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 TESTS_SMT_COMP_DIR="${SCRIPT_DIR}/smt-comp"
-EXPECT_SMT_COMP_DIR=${TESTS_SMT_COMP_DIR}/expect
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 
 exitcode=0
+
+die()
+{
+  echo "$1"
+  exit 1
+}
 
 runtest()
 {
@@ -19,7 +24,7 @@ runtest()
 		echo ${test}
 		tname=`basename $test`
 		tname=${tname%.*}
-		result=$(diff <($2  ${test} $3) ${EXPECT_SMT_COMP_DIR}/${tname}.$3.$4.expect)
+		result=$(diff <($2  ${test} $3) $1/expect/${tname}.$3.$4.expect)
 		if [ ! -z "$result" ]
     then
 			echo -e "${RED}error:${NOCOLOR} Difference between expected and actual result:"
@@ -29,18 +34,21 @@ runtest()
 	done
 }
 
+[ -d "${TESTS_SMT_COMP_DIR}" ] || die "directory '${TESTS_SMT_COMP_DIR}' does not exist"
+[ -d "${TESTS_SMT_COMP_DIR}/expect" ] || die "directory '${TESTS_SMT_COMP_DIR}/expect' does not exist"
+
 echo "Run single-query/industry challenge track scrambler..."
-runtest ${TESTS_SMT_COMP_DIR} ${SCRIPT_DIR}/../process.single-query-challenge-track 0 single
-runtest ${TESTS_SMT_COMP_DIR} ${SCRIPT_DIR}/../process.single-query-challenge-track 1234 single
+runtest "${TESTS_SMT_COMP_DIR}" "${SCRIPT_DIR}"/../process.single-query-challenge-track 0 single
+runtest "${TESTS_SMT_COMP_DIR}" "${SCRIPT_DIR}"/../process.single-query-challenge-track 1234 single
 echo -e "\nRun incremental track scrambler..."
-runtest ${TESTS_SMT_COMP_DIR} ${SCRIPT_DIR}/../process.incremental-track 0 incremental
-runtest ${TESTS_SMT_COMP_DIR} ${SCRIPT_DIR}/../process.incremental-track 1234 incremental
+runtest "${TESTS_SMT_COMP_DIR}" "${SCRIPT_DIR}"/../process.incremental-track 0 incremental
+runtest "${TESTS_SMT_COMP_DIR}" "${SCRIPT_DIR}"/../process.incremental-track 1234 incremental
 echo -e "\nRun unsat-core track scrambler..."
-runtest ${TESTS_SMT_COMP_DIR} ${SCRIPT_DIR}/../process.unsat-core-track 0 unsat-core
-runtest ${TESTS_SMT_COMP_DIR} ${SCRIPT_DIR}/../process.unsat-core-track 1234 unsat-core
+runtest "${TESTS_SMT_COMP_DIR}" "${SCRIPT_DIR}"/../process.unsat-core-track 0 unsat-core
+runtest "${TESTS_SMT_COMP_DIR}" "${SCRIPT_DIR}"/../process.unsat-core-track 1234 unsat-core
 echo -e "\nRun model-validation track scrambler..."
-runtest ${TESTS_SMT_COMP_DIR} ${SCRIPT_DIR}/../process.model-val-track 0 model-val
-runtest ${TESTS_SMT_COMP_DIR} ${SCRIPT_DIR}/../process.model-val-track 1234 model-val
+runtest "${TESTS_SMT_COMP_DIR}" "${SCRIPT_DIR}"/../process.model-val-track 0 model-val
+runtest "${TESTS_SMT_COMP_DIR}" "${SCRIPT_DIR}"/../process.model-val-track 1234 model-val
 
 if [ $exitcode -ne 0 ]
 then
